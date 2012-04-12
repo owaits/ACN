685e80de42c64f1bed0a19c506fefb4de5b2b5e8
MarkDaniel_cp <SND\MarkDaniel_cp>
2012-04-12 15:31:57 +0000

    Added basic SNTP implimentation with sample client and server

---
+++ Acn/SntpUpdate/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Acn.Sntp;
+using NDesk.Options;
+
+namespace SntpUpdate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string serverAddress = "ntp2d.mcc.ac.uk";
+            bool addressExplicit = false;
+            bool setSystemTime = false;
+            bool help = false;
+
+            var p = new OptionSet() {
+                { "s=|server=", "Server to request time from",  v => { serverAddress = v; addressExplicit = true; } },
+                { "u|update", "Update the system clock with the recieved time", v => setSystemTime = v != null},
+                { "h|?|help", "Print command line help",  v => help = v != null }
+            };
+
+            List<string> extra;
+            try
+            {
+                extra = p.Parse(args);
+            }
+            catch (OptionException e)
+            {
+                Console.Write("SntpUpdate: ");
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Try `SntpUpdate --help' for more information.");
+                return;
+            }
+
+            if (!addressExplicit && extra.Count > 0)
+            {
+                serverAddress = extra[0];
+            }
+
+            if (help)
+                p.WriteOptionDescriptions(Console.Out);
+            else
+            {
+                SntpClient client = new SntpClient(serverAddress);
+                NtpData data = client.GetTime(setSystemTime);
+                Console.WriteLine(data.ToString());
+            }
+        }
+    }
+}
--- Acn/SntpUpdate/Properties/AssemblyInfo.cs
+++ Acn/SntpUpdate/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Acn")]
+[assembly: AssemblyTitle("SntpUpdate")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("ACN Project")]
-[assembly: AssemblyProduct("Acn")]
-[assembly: AssemblyCopyright("Copyright © Oliver Waits et al, 2011")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SntpUpdate")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4caf1ec4-0199-4513-8dc2-207acc266cd2")]
+[assembly: Guid("1228156e-2b91-4ebe-b3e0-8a4eb3d3e031")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,7 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
-
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Acn.Test")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
