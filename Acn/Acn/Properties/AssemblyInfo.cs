fb00973a9428fdf75a3f874fbd63b9b503b14710
owaits_cp <SND\owaits_cp>
2014-08-01 09:07:03 +0000

    Put version in single common file.

--- Acn/Acn/Properties/AssemblyInfo.cs
+++ Acn/Acn/Properties/AssemblyInfo.cs
@@ -22,17 +22,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("4caf1ec4-0199-4513-8dc2-207acc266cd2")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.21.0")]
-[assembly: AssemblyFileVersion("1.1.21.0")]
 
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Acn.Test")]
---
+++ Acn/ProductVersion.cs
@@ -0,0 +1,14 @@
+﻿//___________________________________________________________________________
+// ProductVersion
+// 
+// Author: Oliver Waits
+// Copyright © Avolites Ltd 2009
+//___________________________________________________________________________
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("1.2.1.0")]
+[assembly: AssemblyFileVersion("1.2.1.0")]
+
