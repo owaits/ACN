a25c953b46cd204b0c3acfbbebfae32912d756c1
gregoryhaynes_cp <SND\gregoryhaynes_cp>
2017-01-24 16:56:06 +0000

    New build for changes to AcnSocket Close method.

--- Acn/ProductVersion.cs
+++ Acn/ProductVersion.cs
@@ -9,6 +9,6 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.2.13.0")]
-[assembly: AssemblyFileVersion("1.2.13.0")]
+[assembly: AssemblyVersion("1.2.14.0")]
+[assembly: AssemblyFileVersion("1.2.14.0")]
 
