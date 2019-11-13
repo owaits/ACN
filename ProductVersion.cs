5e7168410e086d50417409e5501c20b235c3aa74
Oliver Waits <owaits@gmail.com>
2019-11-13 17:11:23 +0000

    Published Version 2.0 Alpha 4
    Updated version number to V2.0 Alpha 4

--- ProductVersion.cs
+++ ProductVersion.cs
@@ -9,6 +9,6 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("2.0.3.0")]
-[assembly: AssemblyFileVersion("2.0.3.0")]
+[assembly: AssemblyVersion("2.0.4.0")]
+[assembly: AssemblyFileVersion("2.0.4.0")]
 
