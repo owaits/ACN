565ea1d01a1dc431e1fee3b254446eec4df0a05a
Oliver Waits <owaits@gmail.com>
2021-06-14 13:42:06 +0100

    Update version to V3.0.3.7

--- ProductVersion.cs
+++ ProductVersion.cs
@@ -9,6 +9,6 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("3.0.3.6")]
-[assembly: AssemblyFileVersion("3.0.3.6")]
+[assembly: AssemblyVersion("3.0.3.7")]
+[assembly: AssemblyFileVersion("3.0.3.7")]
 
