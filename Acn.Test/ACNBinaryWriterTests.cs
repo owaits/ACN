6c61ca4bc4d17856df65f67360aa5caa62547dee
Oliver Waits <owaits@gmail.com>
2023-01-24 17:27:02 +0000

    When writting strings to an ACN packet of a fixed length it now truncates the string if it is too long.

---
+++ Acn.Test/ACNBinaryWriterTests.cs
@@ -0,0 +1,51 @@
+﻿using LXProtocols.Acn.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acn.Test
+{
+    /// <summary>
+    /// Tests for <see cref="AcnBinaryWriter"/>
+    /// </summary>
+    [TestClass]
+    public class ACNBinaryWriterTests
+    {
+        /// <summary>
+        /// Ensures that the ACN method to write a fixed length string to a stream functions correctly.
+        /// </summary>
+        [TestMethod]
+        public void WriteUtf8StringTest()
+        {
+            //Tests that a normal string within the length is written correctly.
+            using (var stream = new MemoryStream())
+            {
+                var writer = new AcnBinaryWriter(stream);
+                writer.WriteUtf8String("ABCD", 5);
+
+                Assert.AreEqual(5, stream.Length);
+
+                var data = stream.ToArray();
+                Assert.AreEqual(65, data[0]);
+                Assert.AreEqual(0, data[4]);
+            }
+
+            //Tests that a normal string that exceeds the length is written correctly.
+            using (var stream = new MemoryStream())
+            {
+                var writer = new AcnBinaryWriter(stream);
+                writer.WriteUtf8String("ABCDEF", 5);
+
+                Assert.AreEqual(5, stream.Length);
+
+                var data = stream.ToArray();
+                Assert.AreEqual(65, data[0]);
+                Assert.AreEqual(69, data[4]);
+            }
+        }
+    }
+}
--- Acn/IO/AcnBinaryWriter.cs
+++ Acn/IO/AcnBinaryWriter.cs
@@ -7,24 +7,48 @@ using System.Net;
 
 namespace LXProtocols.Acn.IO
 {
+    /// <summary>
+    /// Extends the <see cref="BinaryWriter"/> class with methods specific to writting the an ACN stream.
+    /// </summary>
+    /// <seealso cref="System.IO.BinaryWriter" />
     public class AcnBinaryWriter : BinaryWriter
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AcnBinaryWriter"/> class.
+        /// </summary>
+        /// <param name="input">The input.</param>
         public AcnBinaryWriter(Stream input)
             : base(input)
         { }
 
+        /// <summary>
+        /// Writes an ACN Octet to the stream.
+        /// </summary>
+        /// <param name="value">The 16Bit value to write to the stream.</param>
         public void WriteOctet(short value)
         {
             Write(IPAddress.HostToNetworkOrder(value));
         }
 
+        /// <summary>
+        /// Writes an ACN Octet to the stream.
+        /// </summary>
+        /// <param name="value">The 32Bit value to write to the stream.</param>
         public void WriteOctet(int value)
         {
             Write(IPAddress.HostToNetworkOrder(value));
         }
 
+        /// <summary>
+        /// Writes the UTF8 string of a certain length to the stream.
+        /// </summary>
+        /// <param name="value">The string you want to write to the stream.</param>
+        /// <param name="length">The length of the string to write, unused characters will be padded with zeros.</param>
         public void WriteUtf8String(string value, int length)
         {
+            //Limit the length of the string to the length specified.
+            value = value?.Substring(0,Math.Min(value.Length,length));
+
             Write(UTF8Encoding.UTF8.GetBytes(value));
             Write(new byte[(length - value.Length)]);
         }
