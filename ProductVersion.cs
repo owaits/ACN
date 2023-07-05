b1574c90c4f2b67ce1e3b12ec1c16fed2a29deb6
Oliver Waits <owaits@gmail.com>
2023-07-05 11:46:59 +0100

    Bump Version

--- ProductVersion.cs
+++ ProductVersion.cs
@@ -9,6 +9,6 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("3.0.4.1")]
-[assembly: AssemblyFileVersion("3.0.4.1")]
+[assembly: AssemblyVersion("3.0.4.2")]
+[assembly: AssemblyFileVersion("3.0.4.2")]
 
