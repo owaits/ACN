1b08c0de891fdd89b88b384e4a3b2f3e7f7e9c65
Oliver Waits <owaits@gmail.com>
2024-04-11 20:04:03 +0100

    Update to version 3.0.5

--- ProductVersion.cs
+++ ProductVersion.cs
@@ -9,6 +9,6 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("3.0.4.2")]
-[assembly: AssemblyFileVersion("3.0.4.2")]
+[assembly: AssemblyVersion("3.0.5.2")]
+[assembly: AssemblyFileVersion("3.0.5.2")]
 
